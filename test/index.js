3f32c7a81f2fd93f199550fd27286c6c1fc2cb21
shsxol <shrestha.smile31@gmail.com>
2023-06-16 13:59:13 +0545

    2nd class

---
+++ test/index.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+// using var
+var a="123";
+console.log(a);
+//using let
+let b="456";
+console.log(b);
+//using const
+const c=789;
+console.log(c);
+
+// web based user interacting function
+
+// alert("hello world");
+
+// const age=prompt("how old are you");
+// console.log(age);
+
+// const isBoss= confirm("are the boss?");
+// console.log(isBoss);
+
+
+
