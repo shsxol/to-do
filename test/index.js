56330666bf944e1df9962615f030a5a096406fbd
shsxol <shrestha.smile31@gmail.com>
2023-06-16 14:08:43 +0545

    operators are tested

--- test/index.js
+++ test/index.js
@@ -18,6 +18,25 @@ console.log(c);
 
 // const isBoss= confirm("are the boss?");
 // console.log(isBoss);
+//  commit
+let x=1;
+const y=2;
+if (x==1){
+    console.log("x is 1");
+}
+if (x===1 && y===2){
+    console.log("&& operator is tested");
+}
+
+if ((x=1) || (y=3))
+{
+    console.log("|| is tested");
+}
+
+if (x!=2)
+{
+    console.log("!= operator is tested");
+}
 
 
 
