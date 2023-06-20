781fd75608f1b9d0a0b5e4c9e86164e4ce9c18a7
shsxol <shrestha.smile31@gmail.com>
2023-06-20 13:49:38 +0545

    array

--- test/thirdclass.js
+++ test/thirdclass.js
@@ -7,3 +7,17 @@ const person ={
 };
 
 console.log(person.fullname())
+
+//array
+
+const cars=[ "smile", "stha", {name:"smile", lastname:"stha"}]
+// frist index
+const a = cars[0]
+console.log(a)
+// last index
+const l=cars[cars.length-1]
+console.log(l)
+
+console.log(cars.length)
+console.log(cars.sort())
+
